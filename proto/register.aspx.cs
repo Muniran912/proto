3cc6d86aa240ae0a2e5f719744c6df973bfa3720
Muniran912 <Muniran912@hotmail.com>
2021-03-17 20:01:37 +0800

    Add project files.

---
+++ proto/register.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace proto
+{
+    public partial class register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            //create a dbcontext that specified the connection string
+            var identityDbContext = new IdentityDbContext("IdentityConnectionString");
+            //create user store and user manager
+            var userStore = new UserStore<IdentityUser>(identityDbContext);
+            var manager = new UserManager<IdentityUser>(userStore);
+            //create user
+            var user = new IdentityUser() { UserName = txtRegEmail.Text, Email = txtRegEmail.Text };
+            IdentityResult result = manager.Create(user, txtRegPassword.Text);
+            if (result.Succeeded)
+            {
+                litRegisterError.Text = "Registration Successful ";
+            }
+            else
+            {
+                litRegisterError.Text = "An error has occurred: " + result.Errors.FirstOrDefault();
+            }
+        }
+    }
+}
